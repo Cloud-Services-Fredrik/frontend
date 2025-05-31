a1bc2f5d421ffae81e8fd83af2aa0aba10870aae
Dot-Blader <fredrik.ehrs@outlook.com>
2025-05-31 22:20:32 +0200

    fixed images again

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -4,11 +4,11 @@ const Nav = () => {
     return (
         <nav>
             <div>
-                <img src="/src/assets/images/Logo.png" />
+                <img src="./src/assets/images/Logo.png" />
             </div>
             <div className="flex-row nav-content">
                 <div>
-                    <img src="/src/assets/images/Ticket.png" />
+                    <img src="./src/assets/images/Ticket.png" />
                 </div>
                 <div className="nav-text">Events</div>
             </div>
