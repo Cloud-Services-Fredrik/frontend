c7402387c4c5e524517f03ca09154c2516d73639
Dot-Blader <fredrik.ehrs@outlook.com>
2025-05-31 22:27:16 +0200

    final attempt at fixing images

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -4,11 +4,11 @@ const Nav = () => {
     return (
         <nav>
             <div>
-                <img src="../images/Logo.png" />
+                <img src="/images/Logo.png" />
             </div>
             <div className="flex-row nav-content">
                 <div>
-                    <img src="../images/Ticket.png" />
+                    <img src="/images/Ticket.png" />
                 </div>
                 <div className="nav-text">Events</div>
             </div>
